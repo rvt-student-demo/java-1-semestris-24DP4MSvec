ddef6c0f909b0ed3a96b02de4ceb9b1ff3b190e3
24DP4MSvec <A240932MS@rvt.lv>
2025-09-29 09:50:32 +0300

    Uzdevums 'Delikatese'

---
+++ src/main/java/rvt/delikatese.java
@@ -0,0 +1,36 @@
+package rvt;
+
+import java.util.Scanner;
+public class delikatese {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Ievadi preci: "); 
+        String prece = scanner.nextLine();
+
+        System.out.println("Ievadi cenu: ");
+        double cena = Double.valueOf(scanner.nextLine());
+
+        System.out.println("Ekspress  piegāde (0 - ne, 1 - ja)");
+        int ekspress = Integer.valueOf(scanner.nextLine());
+        double piegade = 0;
+        if (cena < 10) {
+            piegade = 2.00;
+            if (ekspress == 1) {
+                piegade += 3;
+            }
+        }
+        else {
+            piegade = 0;
+            if (ekspress == 1) {
+                piegade += 3;
+            }
+        }
+        double kopa = cena + piegade;
+
+        System.out.println("Rekins: ");
+        System.out.println(prece + " " + cena);
+        System.out.println("Piegade: " + piegade);
+        System.out.println("Kopa: " + kopa);
+    }
+}
